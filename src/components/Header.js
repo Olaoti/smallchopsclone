3ce1ec1f928c86ac2f0a2e96064c17d3bd4db821
Omotola Olaoti <omotolaolaoti@gmail.com>
2022-09-30 00:49:10 +0100

    home-box

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <div className='header'>
-        <div className="header__text">Order small chops for <br/>today, tomorrow or <br/> whenever!</div>
-        <div className="header__btn">Order Now</div>
-        <div className="header__animation">
-            
+    <div className="header">
+      <Navbar />
+      <div className="header__text">
+        Order small chops for <br />
+        today, tomorrow or <br /> whenever!
+      </div>
+      <div className="header__btn">Order Now</div>
+      <div className="header__animation">
+        <div className="openup">
+          <div className="opened">
+            <div className="leftopen"></div>
+            <div className="mainopen"></div>
+            <div className="rightopen"></div>
+          </div>
+          <div className="opentip"></div>
         </div>
-    </div>
-  )
-}
 
-export default Header
+        <div className="relatived">
+          <div className="top">
+            <div className="cover"></div>
+            <div className="covertip"></div>
+          </div>
+          <div className="middle">
+            <div className="left"></div>
+            <div className="mid"></div>
+            <div className="right"></div>
+          </div>
+        </div>
+
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,6 @@ const Home = () => {
   return (
     <div className="home">
       <div className="landing">
-        <Navbar />
         <div className="home__header">
           <Blurhomebg />
           <Header />
